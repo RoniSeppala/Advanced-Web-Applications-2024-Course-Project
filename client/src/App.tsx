9acaebcfc66c09a8fe1a306164fcf8a1bbd0807b
Roni Seppälä <roni.o.seppala@gmail.com>
2025-02-23 20:44:16 +0200

    added a debuging page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,8 +4,11 @@ import Login from './components/Login'
 import Register from './components/Register'
 import Home from "./components/Home"
 import Header from "./components/Header"
+import { ChromePicker } from 'react-color'
+import React from 'react'
 
 function App() {
+  const [color, setColor] = React.useState<string>("#000000")
 
   return (
     <>
@@ -15,6 +18,11 @@ function App() {
             <Route path="/" element={<Home/>} />
             <Route path="/login" element={<Login/>} />
             <Route path="/register" element={<Register />} />
+            <Route path="/chrome" element={
+              <div>
+                <ChromePicker color={color} onChange={(newColor) => {console.log(newColor.hex); setColor(newColor.hex)}}/>
+              </div>
+            } />
             <Route path="*" element={<h1>404: Page not found</h1>} />
           </Routes>
         </BrowserRouter>
